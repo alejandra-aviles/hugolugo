1903bbc4b3553d136eded7042a9eb62f509dbea5
opsb <oliver@opsb.co.uk>
2015-10-06 23:13:56 +0200

    Fix scroll to in firefox and IE

--- source/javascripts/modules/_scroll_to.js
+++ source/javascripts/modules/_scroll_to.js
@@ -2,7 +2,7 @@ jQuery(".scroll-to").click(function(e){
 	e.preventDefault();
 	var element = this;
 	var targetHref = $(element).attr("href");
-	jQuery("body").animate({ scrollTop: $(targetHref).offset().top }, 1000, 'swing');
+	jQuery("body,html").animate({ scrollTop: $(targetHref).offset().top }, 1000, 'swing');
 	jQuery(".active").removeClass("active");
 	jQuery(element).addClass("active");
 });
